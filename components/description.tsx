b2bc18d8c830a554e14b9f604b3dfa81ceb306d9
XiangNorth <i@xiangnorth.com>
2023-03-23 21:31:43 +0800

    feat: Finish description part 1

--- components/description.tsx
+++ components/description.tsx
@@ -1,102 +1,35 @@
-
+import { IconFilter, IconMessageChatbot, IconMessageCircle2Filled, IconSparkles, IconStack2 } from '@tabler/icons-react';
 
 export default function Description() {
     return (<>
-        <div className="container mx-auto">
+        <div className="container mx-auto my-5">
             <div className="flex items-center mt-10 w-full">
-                <div className="w-full md:w-4/5 mr-auto ml-auto flex flex-col items-center justify-center gap-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-16 h-16 fill-primary">
-                        <path d="M5.223 2.25c-.497 0-.974.198-1.325.55l-1.3 1.298A3.75 3.75 0 007.5 9.75c.627.47 1.406.75 2.25.75.844 0 1.624-.28 2.25-.75.626.47 1.406.75 2.25.75.844 0 1.623-.28 2.25-.75a3.75 3.75 0 004.902-5.652l-1.3-1.299a1.875 1.875 0 00-1.325-.549H5.223z" />
-                        <path fillRule="evenodd" d="M3 20.25v-8.755c1.42.674 3.08.673 4.5 0A5.234 5.234 0 009.75 12c.804 0 1.568-.182 2.25-.506a5.234 5.234 0 002.25.506c.804 0 1.567-.182 2.25-.506 1.42.674 3.08.675 4.5.001v8.755h.75a.75.75 0 010 1.5H2.25a.75.75 0 010-1.5H3zm3-6a.75.75 0 01.75-.75h3a.75.75 0 01.75.75v3a.75.75 0 01-.75.75h-3a.75.75 0 01-.75-.75v-3zm8.25-.75a.75.75 0 00-.75.75v5.25c0 .414.336.75.75.75h3a.75.75 0 00.75-.75v-5.25a.75.75 0 00-.75-.75h-3z" clipRule="evenodd" />
-                    </svg>
-                    <h3>信息堆</h3>
-                    <div className="flex justify-center items-center gap-5 flex-col md:flex-row">
+                <div className="w-full md:w-9/10 mr-auto ml-auto flex flex-col items-center justify-center gap-5">
+                    <IconStack2 size={64} stroke={1.5} className="text-primary" />
+                    <div className="font-medium text-2xl">信息堆</div>
+                    <div className="flex justify-center items-center gap-10 flex-col md:flex-row">
                         <div className="card bg-primary prose p-5 w-full md:basis-1/2 text-primary-content">
-                            <h3>什么是「信息堆」？</h3>
+                            <h3 className="self-center text-primary-content text-2xl">什么是「信息堆」？</h3>
                             <p>信息堆是一种新的信息组织和呈现方式，目的是为了帮助用户更快速和便捷地找到“有效信息“。用户可以在信息堆里快速浏览信息，通过各种维度筛选信息，还可以「点亮」信息，让有效信息被更多人看到，或者「回复」信息，与其他用户实时交流。</p>
                             <b>从另一个角度理解:</b>
-                            <p className="text-2xl">「信息堆」= 搜索引擎 + 聊天室 + AI</p>
+                            <p>「信息堆」= 搜索引擎 + 聊天室 + AI</p>
                         </div>
-                        <div className="w-full md:basis-1/2 md:h-full prose">
-                            <div className="flex flex-col md:h-full content-evenly">
-                                <div>
-                                    加入信息堆，实时交流
-                                </div>
-                                <div>
-                                    「点亮」有效信息
-                                </div>
-                                <div>
-                                    「回复」别人信息
-                                </div>
-                                <div>
-                                    多维度筛选，找到有效信息
-                                </div>
+                        <div className="w-full md:basis-1/2 h-full prose flex flex-col gap-10 items-center md:items-baseline">
+                            <div className='flex items-center'>
+                                <IconMessageChatbot size={32} stroke={1.5} className='text-primary' />
+                                加入信息堆，实时交流
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="flex items-center mt-10 w-full">
-                <div className="w-full md:w-4/5 mr-auto ml-auto flex flex-col items-center justify-center gap-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-16 h-16 fill-primary">
-                        <path d="M5.223 2.25c-.497 0-.974.198-1.325.55l-1.3 1.298A3.75 3.75 0 007.5 9.75c.627.47 1.406.75 2.25.75.844 0 1.624-.28 2.25-.75.626.47 1.406.75 2.25.75.844 0 1.623-.28 2.25-.75a3.75 3.75 0 004.902-5.652l-1.3-1.299a1.875 1.875 0 00-1.325-.549H5.223z" />
-                        <path fillRule="evenodd" d="M3 20.25v-8.755c1.42.674 3.08.673 4.5 0A5.234 5.234 0 009.75 12c.804 0 1.568-.182 2.25-.506a5.234 5.234 0 002.25.506c.804 0 1.567-.182 2.25-.506 1.42.674 3.08.675 4.5.001v8.755h.75a.75.75 0 010 1.5H2.25a.75.75 0 010-1.5H3zm3-6a.75.75 0 01.75-.75h3a.75.75 0 01.75.75v3a.75.75 0 01-.75.75h-3a.75.75 0 01-.75-.75v-3zm8.25-.75a.75.75 0 00-.75.75v5.25c0 .414.336.75.75.75h3a.75.75 0 00.75-.75v-5.25a.75.75 0 00-.75-.75h-3z" clipRule="evenodd" />
-                    </svg>
-                    <h3>信息堆</h3>
-                    <div className="flex justify-center items-center gap-5 flex-col md:flex-row">
-                        <div className="card bg-primary prose p-5 w-full md:basis-1/2 text-primary-content">
-                            <h3>什么是「信息堆」？</h3>
-                            <p>信息堆是一种新的信息组织和呈现方式，目的是为了帮助用户更快速和便捷地找到“有效信息“。用户可以在信息堆里快速浏览信息，通过各种维度筛选信息，还可以「点亮」信息，让有效信息被更多人看到，或者「回复」信息，与其他用户实时交流。</p>
-                            <b>从另一个角度理解:</b>
-                            <p className="text-2xl">「信息堆」= 搜索引擎 + 聊天室 + AI</p>
-                        </div>
-                        <div className="w-full md:basis-1/2 md:h-full prose">
-                            <div className="flex flex-col md:h-full content-evenly">
-                                <div>
-                                    加入信息堆，实时交流
-                                </div>
-                                <div>
-                                    「点亮」有效信息
-                                </div>
-                                <div>
-                                    「回复」别人信息
-                                </div>
-                                <div>
-                                    多维度筛选，找到有效信息
-                                </div>
+                            <div className='flex items-center'>
+                                <IconSparkles size={32} stroke={1.5} className='text-primary' />
+                                「点亮」有效信息
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="flex items-center mt-10 w-full">
-                <div className="w-full md:w-4/5 mr-auto ml-auto flex flex-col items-center justify-center gap-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-16 h-16 fill-primary">
-                        <path d="M5.223 2.25c-.497 0-.974.198-1.325.55l-1.3 1.298A3.75 3.75 0 007.5 9.75c.627.47 1.406.75 2.25.75.844 0 1.624-.28 2.25-.75.626.47 1.406.75 2.25.75.844 0 1.623-.28 2.25-.75a3.75 3.75 0 004.902-5.652l-1.3-1.299a1.875 1.875 0 00-1.325-.549H5.223z" />
-                        <path fillRule="evenodd" d="M3 20.25v-8.755c1.42.674 3.08.673 4.5 0A5.234 5.234 0 009.75 12c.804 0 1.568-.182 2.25-.506a5.234 5.234 0 002.25.506c.804 0 1.567-.182 2.25-.506 1.42.674 3.08.675 4.5.001v8.755h.75a.75.75 0 010 1.5H2.25a.75.75 0 010-1.5H3zm3-6a.75.75 0 01.75-.75h3a.75.75 0 01.75.75v3a.75.75 0 01-.75.75h-3a.75.75 0 01-.75-.75v-3zm8.25-.75a.75.75 0 00-.75.75v5.25c0 .414.336.75.75.75h3a.75.75 0 00.75-.75v-5.25a.75.75 0 00-.75-.75h-3z" clipRule="evenodd" />
-                    </svg>
-                    <h3>信息堆</h3>
-                    <div className="flex justify-center items-center gap-5 flex-col md:flex-row">
-                        <div className="card bg-primary prose p-5 w-full md:basis-1/2 text-primary-content">
-                            <h3>什么是「信息堆」？</h3>
-                            <p>信息堆是一种新的信息组织和呈现方式，目的是为了帮助用户更快速和便捷地找到“有效信息“。用户可以在信息堆里快速浏览信息，通过各种维度筛选信息，还可以「点亮」信息，让有效信息被更多人看到，或者「回复」信息，与其他用户实时交流。</p>
-                            <b>从另一个角度理解:</b>
-                            <p className="text-2xl">「信息堆」= 搜索引擎 + 聊天室 + AI</p>
-                        </div>
-                        <div className="w-full md:basis-1/2 md:h-full prose">
-                            <div className="flex flex-col md:h-full content-evenly">
-                                <div>
-                                    加入信息堆，实时交流
-                                </div>
-                                <div>
-                                    「点亮」有效信息
-                                </div>
-                                <div>
-                                    「回复」别人信息
-                                </div>
-                                <div>
-                                    多维度筛选，找到有效信息
-                                </div>
+                            <div className='flex items-center'>
+                                <IconMessageCircle2Filled size={32} stroke={1.5} className='text-primary' />
+                                「回复」别人信息
+                            </div>
+                            <div className='flex items-center'>
+                                <IconFilter size={32} stroke={1.5} className='text-primary' />
+                                多维度筛选，找到有效信息
                             </div>
                         </div>
                     </div>
