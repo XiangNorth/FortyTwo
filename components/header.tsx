a8928d2a1763b56d9b151947f87ae9735f3d82eb
XiangNorth <i@xiangnorth.com>
2023-03-23 17:37:35 +0800

    style: Adjust header style for better navigation

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 fixed z-30">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
